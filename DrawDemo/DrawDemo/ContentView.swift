391f83402529e1298fa17d279f89db148c7cefaa
Jihunkim95 <darak551@naver.com>
2023-12-19 10:33:04 +0900

    add: 베지어 곡선 추가

--- DrawDemo/DrawDemo/ContentView.swift
+++ DrawDemo/DrawDemo/ContentView.swift
@@ -71,10 +71,16 @@ struct PathView: View {
             path.move(to: CGPoint(x: 10, y: 0))
             path.addLine(to: CGPoint(x: 10, y: 350))
             path.addLine(to: CGPoint(x: 300, y: 300))
+            //2차 베지어 곡선
+            path.addQuadCurve(to: CGPoint(x: 200, y: 200), control: CGPoint(x: 100, y: 200))
+            //3차 베지어 곡선
+            path.addCurve(to: CGPoint(x:10,y: 10), control1: CGPoint(x: 100, y: 200), control2: CGPoint(x: 100, y: 200))
         }
         .fill(.green)
     }
 }
+
+
 #Preview {
     ContentView()
 }
