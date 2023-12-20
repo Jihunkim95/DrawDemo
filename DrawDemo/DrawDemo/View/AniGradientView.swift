ba12464f4be61f41294a0769fc22c11874076b2a
Jihunkim95 <darak551@naver.com>
2023-12-20 11:30:07 +0900

    add: 선형, 원뿔 그라데이션 애니메이션 추가

--- DrawDemo/DrawDemo/View/AniGradientView.swift
+++ DrawDemo/DrawDemo/View/AniGradientView.swift
@@ -9,7 +9,26 @@ import SwiftUI
 
 struct AniGradientView: View {
     // 애니메이션 상태를 유지하는 상태 변수
-    @State private var animateGradient = false
+    @State var animateGradient = false
+
+    var body: some View {
+        //선형
+        LinearGradientView(animateGradient: $animateGradient)
+        
+        //원뿔
+        RadialGradientView(animateGradient: $animateGradient)
+    }
+}
+
+#Preview {
+    AniGradientView()
+}
+
+
+struct LinearGradientView: View {
+    // 애니메이션 상태를 유지하는 상태 변수
+    @Binding var animateGradient:Bool
+    
     var body: some View {
         //왼쪽 상단에서 오른쪽 하단 애니메이션
         // 애니메이션이 시작되면 시작점을 왼쪽 상단에서 왼쪽 하단으로 변경하고, 종료점을 오른쪽 하단에서 오른쪽 상단으로 변경
@@ -27,6 +46,24 @@ struct AniGradientView: View {
     }
 }
 
-#Preview {
-    AniGradientView()
+struct RadialGradientView: View {
+    // 애니메이션 상태를 유지하는 상태 변수
+    @Binding var animateGradient:Bool
+    
+    var body: some View {
+        //왼쪽 상단에서 오른쪽 하단 애니메이션
+        // 애니메이션이 시작되면 시작점을 왼쪽 상단에서 왼쪽 하단으로 변경하고, 종료점을 오른쪽 하단에서 오른쪽 상단으로 변경
+        RadialGradient(colors:  [Color(red: 195 / 255.0, green: 237 / 255.0, blue: 232 / 255.0),
+                                 Color(red: 236 / 255.0, green: 246 / 255.0, blue: 238 / 255.0),
+                                 Color(red: 239 / 255.0, green: 207 / 255.0, blue: 246 / 255.0)],
+                           center: .center,
+                               startRadius: animateGradient ? 400 : 200,
+                               endRadius:   animateGradient ? 20 : 40)
+            .ignoresSafeArea()
+            .onAppear{
+                withAnimation(.linear(duration: 3).repeatForever(autoreverses: true)) {
+                    animateGradient.toggle()
+                }
+            }
+    }
 }
